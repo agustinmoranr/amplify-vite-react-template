d148e705d516940c77333494aa89b6764e9a0fa1
Agustin Moran <agustin.moranr@gmail.com>
2024-06-06 12:26:30 -0600

    fix: quickstart per user auth. Shows correctly the data for the user logged in when changing between accounts.

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,6 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
 
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
@@ -21,11 +14,7 @@ function App() {
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+      <TodoList />
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
@@ -38,3 +27,21 @@ function App() {
 }
 
 export default App;
+
+function TodoList() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }, []);
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.content}</li>
+      ))}
+    </ul>
+  )
+}
